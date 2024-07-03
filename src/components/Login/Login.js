528b00bcef0229b838b7f96fa4e4e5d544fe0c2f
ducduy1102 <ducduy1110u@gmail.com>
2024-07-03 10:47:49 +0700

    React router dom

---
+++ src/components/Login/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Login = (props) => {
+  return (
+    <div className="login-container">
+      Login Page
+      <button className="btn btn-primary">Test boostrap</button>
+    </div>
+  );
+};
+
+export default Login;
