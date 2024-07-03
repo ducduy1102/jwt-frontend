95dc2159a06eec453af0069fc7c94c6b1ec78fc8
ducduy1102 <ducduy1110u@gmail.com>
2024-07-03 18:56:02 +0700

    Form login responsive

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,17 +3,18 @@ import "./Login.scss";
 
 const Login = (props) => {
   return (
-    <div className="mt-3 login-container">
+    <div className="login-container">
       <div className="container">
-        <div className="row">
-          <div className="content-left col-6">
+        <div className="px-3 row px-sm-0">
+          <div className="content-left col-sm-6 d-none d-sm-block">
             <div className="brand">Evil Shadow</div>
             <div className="detail">
               Evil Shadow helps you connect and share
             </div>
           </div>
 
-          <div className="gap-3 py-3 flex-column content-right col-6 green d-flex">
+          <div className="gap-3 py-3 flex-column content-right col-sm-6 green d-flex">
+            <div className="text-center brand d-sm-none">Evil Shadow</div>
             <input
               type="text"
               className="form-control"
@@ -25,7 +26,11 @@ const Login = (props) => {
               className="form-control"
             />
             <button className="btn btn-primary">Login</button>
-            <span className="text-center">Forgot your password?</span>
+            <span className="text-center">
+              <a className="forgot-password" href="#">
+                Forgot your password?
+              </a>
+            </span>
             <hr />
             <div className="text-center">
               <button className="btn btn-success ">Create new account</button>
