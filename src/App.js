ffa16701d181a8d771db342508e8746b3794100f
ducduy1102 <ducduy1110u@gmail.com>
2024-07-03 11:31:20 +0700

    Form Login, content-right

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.scss";
 import Login from "./components/Login/Login";
 import Nav from "./components/Navigation/Nav";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,38 @@
 import React from "react";
+import "./Login.scss";
 
 const Login = (props) => {
   return (
-    <div className="login-container">
-      Login Page
-      <button className="btn btn-primary">Test boostrap</button>
+    <div className="mt-3 login-container">
+      <div className="container">
+        <div className="row">
+          <div className="content-left col-6">
+            <div className="brand">Evil Shadow</div>
+            <div className="detail">
+              Evil Shadow helps you connect and share
+            </div>
+          </div>
+
+          <div className="gap-3 py-3 flex-column content-right col-6 green d-flex">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Email address or phone number"
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              className="form-control"
+            />
+            <button className="btn btn-primary">Login</button>
+            <span className="text-center">Forgot your password?</span>
+            <hr />
+            <div className="text-center">
+              <button className="btn btn-success ">Create new account</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
