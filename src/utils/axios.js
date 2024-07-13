13121bf27e4cd6a0fab52314c2015c817f81664d
ducduy1102 <ducduy1110u@gmail.com>
2024-07-13 21:19:26 +0700

    JWT Middleware

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { toast } from "react-toastify";
 
 // Set config defaults when creating the instance
 const instance = axios.create({
@@ -7,6 +8,8 @@ const instance = axios.create({
 
 // Alter defaults after instance has been created
 // instance.defaults.headers.common["Authorization"] = "AUTH_TOKEN Evil";
+
+instance.defaults.withCredentials = true;
 
 // Add a request interceptor
 instance.interceptors.request.use(
@@ -27,10 +30,50 @@ instance.interceptors.response.use(
     // Do something with response data
     return response.data;
   },
-  function (error) {
+  function (err) {
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
-    return Promise.reject(error);
+    const status = err?.response?.status || 500;
+    // console.log(status);
+    switch (status) {
+      // authentication (token related issues)
+      case 401: {
+        toast.error("Unauthorized the user. Please login...");
+        // window.location.href = "/login";
+        return Promise.reject(err);
+      }
+
+      // forbidden (permission related issues)
+      case 403: {
+        toast.error("You don't have permisson to access this resource...");
+        return Promise.reject(err);
+      }
+
+      // bad request
+      case 400: {
+        return Promise.reject(err);
+      }
+
+      // not found
+      case 404: {
+        return Promise.reject(err);
+      }
+
+      // conflict
+      case 409: {
+        return Promise.reject(err);
+      }
+
+      // unprocessable
+      case 422: {
+        return Promise.reject(err);
+      }
+
+      // generic api error (server related) unexpected
+      default: {
+        return Promise.reject(err);
+      }
+    }
   }
 );
 
